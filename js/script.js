f711ea74f87a95b5f5c3085f3ded6048f65ee91a
carloscasara <caducasara98@gmail.com>
2021-07-15 20:39:54 -0300

    definir fim do jogo

--- js/script.js
+++ js/script.js
@@ -50,7 +50,7 @@ function update(event){
 }
 
 function start(){
-    
+
     if(snake[0].x > 15*box  && direction == "right"){
         snake[0].x = 0;
     }
@@ -65,6 +65,14 @@ function start(){
 
     if(snake[0].y < 0  && direction == "up"){
         snake[0].y = 16 * box;
+    }
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(start);
+            alert("Game Over!");
+            alert("Pressione F5 para jogar novamente");
+        }
     }
     
     criarBG();
