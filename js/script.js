8d77d3b6d431ad60f37c4df02306b6f3ed62ed59
carloscasara <caducasara98@gmail.com>
2021-07-15 20:29:28 -0300

    definir aleatoriedade da fruta e tamanho da cobrinha

--- js/script.js
+++ js/script.js
@@ -90,7 +90,14 @@ function start(){
         snakeY += box;
     }
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
+
+
 
     let newHead = {
         x: snakeX,
