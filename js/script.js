19698268ab38dd588e87b0a461b45bad08612f50
carloscasara <caducasara98@gmail.com>
2021-07-15 20:15:11 -0300

    criandop funcao que cria comida e exibe e pontos aleatórios

--- js/script.js
+++ js/script.js
@@ -7,6 +7,10 @@ snake[0] = {
     y: 8 * box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG(){
     context.fillStyle = "lightgreen";
@@ -18,6 +22,11 @@ function criarCobrinha(){
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function drawfood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -60,6 +69,7 @@ function start(){
     
     criarBG();
     criarCobrinha();
+    drawfood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
